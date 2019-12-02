ea599785c21245e3f6d09010a5af830206a082d6
vagrant <vagrant@vagrant.vm>
2019-12-02 16:45:23 +0000

    Initial release

---
+++ README.md
@@ -0,0 +1,13 @@
+# Nubesoplayer for Elgg 2.3.xx
+![alt text](./graphics/nubesoplayer.png "Title Text")
+
+
+## Installation
+
+* Clone this repository: https://github.com/rjcalifornia/nubesoplayer.git
+* Sign into Elgg with the Administrator account
+* Open the Administration Dashboard
+* Go to menu "Configure -> Plugins"
+* Make sure the Files plugin is activated
+* Place the Nubeso Player plugin at the end of the plugin list and activate it
+* Run upgrade.php
---
+++ views/default/nubesoplayer/video-js.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Nubeso Video Player
+ * @package nubesoplayer
+ */
+
+elgg_load_js('nubesoplayer.video-js');
+elgg_load_css('nubesoplayer-video-js');
+
+$video_url = elgg_get_site_url() . "mod/file/download.php?file_guid={$vars['file_guid']}";
+
+?>
+<center>
+    <p>
+  <video id="<?php echo $vars['file_guid']; ?>" class="video-js vjs-default-skin" controls preload="none" width="450" height="300"
+      data-setup="{}">
+    <source src="<?php echo $video_url;?>" type="video/<?php echo $vars['type']; ?>" />
+  </video>
+        </p>
+
+    </center>
+
+</br>
